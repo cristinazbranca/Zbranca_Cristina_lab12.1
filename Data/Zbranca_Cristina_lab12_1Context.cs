83e893848e963d7499cd5a9652d8f989dfbc6e9d
zbranca cristina <zbrancacristina@gmail.com>
2022-01-17 00:00:43 +0200

    Add project files.

---
+++ Data/Zbranca_Cristina_lab12_1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zbranca_Cristina_lab12._1.Models;
+
+namespace Zbranca_Cristina_lab12._1.Data
+{
+    public class Zbranca_Cristina_lab12_1Context : DbContext
+    {
+        public Zbranca_Cristina_lab12_1Context (DbContextOptions<Zbranca_Cristina_lab12_1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Zbranca_Cristina_lab12._1.Models.ShopList> ShopList { get; set; }
+    }
+}
---
+++ Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zbranca_Cristina_lab12._1.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
